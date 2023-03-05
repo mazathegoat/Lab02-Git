4a26784de0f6616b8e356536e878eae89d391c2d
avila <avila@IMPERIAL>
2023-03-04 18:27:37 -0800

    done

--- Lab2/src/gith/Hello.java
+++ Lab2/src/gith/Hello.java
@@ -7,7 +7,7 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("301397577 - This line was added by Alexis Aguilar.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("301277171 - This line was added by Phillip Avila.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
