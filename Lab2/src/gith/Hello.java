2b915df12a1e23b4e5406cd44cb197c012d415c8
bhert11 <111545268+bhert11@users.noreply.github.com>
2023-03-04 22:04:24 -0800

    Done

--- Lab2/src/gith/Hello.java
+++ Lab2/src/gith/Hello.java
@@ -8,7 +8,7 @@ public class Hello {
 		// Add a new line with your information
 		System.out.println("301397577 - This line was added by Alexis Aguilar.");
 		System.out.println("301277171 - This line was added by Phillip Avila.");
-		System.out.println("219993957 - This line was added by Brian Hert");
+		System.out.println("219993957 - This line was added by Brian Hert.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
