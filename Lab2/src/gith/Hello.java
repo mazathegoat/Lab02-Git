6ad28c59f72e1192e06e7c9f9bfa807bc184f2c6
Matthew Morello <mmorello@csus.edu>
2023-03-05 21:07:53 -0800

    Added Matthew Morello

--- Lab2/src/gith/Hello.java
+++ Lab2/src/gith/Hello.java
@@ -9,7 +9,7 @@ public class Hello {
 		System.out.println("301397577 - This line was added by Alexis Aguilar.");
 		System.out.println("301277171 - This line was added by Phillip Avila.");
 		System.out.println("219993957 - This line was added by Brian Hert.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("303243174 - This line was added by Matthew Morello.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
